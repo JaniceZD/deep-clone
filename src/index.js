9235f2986d51a633da95f0064c785f7aa9e9b2d6
JaniceZD <mnzd123@gmail.com>
2020-07-29 14:40:28 +0800

    完成深拷贝

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
+let cache = [] //缓存初始化
+
 function deepClone(source) {
+  //先判断是否是Object类型
+  if (source instanceof Object) {
+    //判断source是否在缓存里
+    let cacheDist = findCache(source)
+    if (cacheDist) {
+      return cacheDist
+    } else {
+      let dist
+      if (source instanceof Array) {
+        dist = new Array()
+      } else if (source instanceof Function) {
+        dist = function () {
+          return source.apply(this, arguments)
+        }
+      } else if (source instanceof Date) {
+        dist = new Date(source)
+      } else if (source instanceof RegExp) {
+        dist = new RegExp(source.source, source.flags)
+      } else {
+        dist = new Object()
+      }
+      cache.push([source, dist])
+      //遍历每个属性克隆
+      for (let key in source) {
+        if (source.hasOwnProperty(key)) {
+          dist[key] = deepClone(source[key])
+        }
+      }
+      return dist
+    }
+  }
   return source
+}
+
+//查找缓存
+function findCache(source) {
+  for (let i = 0; i < cache.length; i++) {
+    if (cache[i][0] === source) {
+      return cache[i][1]
+    }
+  }
+  return undefined
 }
 
 module.exports = deepClone
